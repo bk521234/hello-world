b086b804546f25c34de3d1e82baa6a90ae706beb
bart <bryan.kielpinski52@gmail.com>
2020-03-26 20:42:14 -0600

    adding inital tokens for monkey lang.

---
+++ monkey_interpreter/src/monkey/token/token.go
@@ -0,0 +1,37 @@
+// Package token - token/token.go
+package token
+
+type TokenType string
+
+// Token stucture is used to build
+// takens while lexing source code.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	INDENT = "INDENT" // INDENT add, foobar, x, y ...
+	INT    = "INT"    // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
